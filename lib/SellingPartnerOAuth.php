4af3d3eae568e0771139cf89e04213c964376b29
Moritz Schroeder <moritz.schroeder@molabs.de>
2021-05-18 11:26:42 +0530

    Added method "getAccessTokenByScope" for grantless access tokens

--- lib/SellingPartnerOAuth.php
+++ lib/SellingPartnerOAuth.php
@@ -5,9 +5,14 @@ namespace ClouSale\AmazonSellingPartnerAPI;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\RequestOptions;
+use InvalidArgumentException;
 
 class SellingPartnerOAuth
 {
+
+    public const SCOPE_MIGRATION = 'sellingpartnerapi::migration';
+    public const SCOPE_NOTIFICATIONS = 'sellingpartnerapi::notifications';
+
     /**
      * @param $refreshToken
      * @param $clientId
@@ -36,6 +41,9 @@ class SellingPartnerOAuth
 
         $body = $response->getBody()->getContents();
         $bodyAsJson = json_decode($body, true);
+        if (isset($bodyAsJson['error_description'])) {
+            throw new SellingPartnerOAuthException($bodyAsJson['error_description'], $bodyAsJson['error']);
+        }
 
         return $bodyAsJson['access_token'];
     }
@@ -75,5 +83,43 @@ class SellingPartnerOAuth
         }
 
         return $bodyAsJson['refresh_token'];
+    }
+    /**
+     * @param string $scope
+     * @param string $clientId
+     * @param string $clientSecret
+     *
+     * @throws GuzzleException
+     */
+    public static function getAccessTokenByScope(string $scope, string $clientId, string $clientSecret): ?string
+    {
+        // verify the required parameter 'scope' is set
+        if (!in_array($scope, [self::SCOPE_MIGRATION, self::SCOPE_NOTIFICATIONS])) {
+            throw new InvalidArgumentException('Invalid required parameter $scope when calling getAccessTokenByScope');
+        }
+        $client = new Client();
+        $params = [
+            'grant_type' => 'client_credentials',
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'scope' => 'sellingpartnerapi::migration'
+        ];
+        $options = array_merge([
+            RequestOptions::HEADERS => ['Accept' => 'application/json'],
+            RequestOptions::HTTP_ERRORS => false,
+            'curl' => [
+                CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
+            ],
+        ], $params ? [RequestOptions::FORM_PARAMS => $params] : []);
+
+        $response = $client->request('POST', 'https://api.amazon.com/auth/o2/token', $options);
+
+        $body = $response->getBody()->getContents();
+        $bodyAsJson = json_decode($body, true);
+        if (isset($bodyAsJson['error_description'])) {
+            throw new SellingPartnerOAuthException($bodyAsJson['error_description'], $bodyAsJson['error']);
+        }
+
+        return $bodyAsJson['access_token'];
     }
 }
